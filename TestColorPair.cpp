f3be90cbe183fb729d4305f9c7b287f35e32a8d4
S <shivaraja.s@philips.com>
2024-08-01 00:06:03 +0530

    Fix limit size failure

--- TestColorPair.cpp
+++ TestColorPair.cpp
@@ -1,38 +1,28 @@
 #include <iostream>
 #include"TestColorPair.h"
 
-void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor)
-{
+void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor){
     TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
     std::cout << "Got pair " << colorPair.ToString() << std::endl;
     assert_msg(colorPair.getMajor() == expectedMajor,"Mismatch in the Major color");
     assert_msg(colorPair.getMinor() == expectedMinor,"Mismatch in the Minor color");
 }
 
-void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber)
-{
+void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber){
     int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert_msg(pairNumber == expectedPairNumber,"Mismatch in pair number");
 }
 
-void TestPrintReferenceManual()
-{
-    // Redirect std::cout to a string stream
+void TestPrintReferenceManual(){
     std::ostringstream oss;
     std::streambuf* p_cout_streambuf = std::cout.rdbuf();
     std::cout.rdbuf(oss.rdbuf());
 
-    // Call the function to test
     TelCoColorCoder::PrintReferenceManual();
 
-    // Restore original std::cout buffer
     std::cout.rdbuf(p_cout_streambuf);
-
-    // Get the output as a string
     std::string output = oss.str();
-
-    // Expected output
     std::string expectedOutput =
         "Color Pair Reference Manual\n"
         "1 White Blue\n"
@@ -61,6 +51,5 @@ void TestPrintReferenceManual()
         "24 Violet Brown\n"
         "25 Violet Slate\n";
 
-    // Perform the assertion
     assert_msg(output == expectedOutput,"The reference manual is not as expected.");
 }
